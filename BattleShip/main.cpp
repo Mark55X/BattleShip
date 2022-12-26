5fd91c53b3cae24287c461376e83f16489d468d6
Marco <marco2002stefani@gmail.com>
2022-12-26 08:06:02 +0100

    modifiche a titolo

--- BattleShip/main.cpp
+++ BattleShip/main.cpp
@@ -12,24 +12,24 @@ using battle_ships::GameManager;
 
 /*
 * Pensieri miei abbastanza profondi da scrivere nel readme.TXT
-* 
+*
 * Secondo me è ESSENZIALE dividere il MAIN dal resto del progetto
 * tale che si possono creare molti modi per fare il main
 * senza che influisca DIRETTAMENTE nella logica del gioco.
-* 
+*
 * Mi spiego meglio: perchè il main dovrebbe fare i controlli di validità dei comandi?
-* Per me non deve farli, non è il suo ruolo. Lui sa solo che è un comando, non sa 
+* Per me non deve farli, non è il suo ruolo. Lui sa solo che è un comando, non sa
 * nemmeno cosa fa, perchè la descrizione di tale esecuzione deve essere descritta dal
 * CORE del gioco che mi invierà un oggetto di tipo RESPONSE
-* 
+*
 * Attenzione, parte importante perchè il RESPONSE sarà una risponsa GENERICA (non è un template attenzione :) )
 * Response{
 	StatusExecution : bool  -> Se avvenuto con successo o meno
-	Content : string        -> Contenuto (può essere la string aformattata per il comando XX XX 
+	Content : string        -> Contenuto (può essere la string aformattata per il comando XX XX
 										  o la descrizione tipo 'Eseguito FUOCO su cella nemica A1 ')
 	NextShift : bool        -> False se il turno è ancora del giocatore che ha eseguito il comando
   }
-  
+
   QUESTO PERCHE' giustamente se io avessi i metodi che restituiscono bool cosa capirei?
   Se è false, per quale motivo è false? Coordiante fuori? Entrambe? O solo una? Oppure sono giste
   ma l'origin riguarda nessun unità navale e per me QUESTO NON FA LO SWITCH DEL TURNO!!
@@ -40,31 +40,53 @@ using battle_ships::GameManager;
   Se siamo nel caso di Logger/Replay è palese l'uso delle eccezioni: il file non esiste, problemi di scrittura
   ecc,... MA NEL GIOCO?? Forse si, forse no: bloccare l'esecuzione di colpo mi implicherebbe che non serve più
   la Response descritta qui sopra perchè so che se c'e un ecezzione TEORICAMENTE devo rifare il turno...
-  ma se tipo mi parte un ecezzione per il Logger? Quella viene catturata da sé e quindi non influenzerebbe 
+  ma se tipo mi parte un ecezzione per il Logger? Quella viene catturata da sé e quindi non influenzerebbe
   l'andamento della partita...dovrebbe bloccare la partita? Come ? Vado a lavarmi, buon Natale a tutti.
 
-  Abbiamo quindi: Eccezioni ->  PRO: Dati che si passano più leggeri(o così si crede)  
-								CONTRO:blocco dell'esecuzione improvviso 
+  Abbiamo quindi: Eccezioni ->  PRO: Dati che si passano più leggeri(o così si crede)
+								CONTRO:blocco dell'esecuzione improvviso
 
 				  Response ->   PRO: Non c'è interruzione del flusso (forse un pò stile retro?)
 								CONTRO: La gran parte delle funzioni resituiscono la response
 
   Attenzione che... XX XX SUCEESFUL ma non cambia turno..come faccio con ecezzioni?
 					A1 B2 SUCESFUL fantastico...nesusna ecezzione ma come stampo che è avvenuta l'azione
-	Quello che sto dicendo è che: io ho lo stesso risultato di Response SOLO se avviene l'eccezione. Ma 
+	Quello che sto dicendo è che: io ho lo stesso risultato di Response SOLO se avviene l'eccezione. Ma
 	io magari vorrei avere il risultato anche quando non accade. OVVERO dare più potere al main,
 	fargli capire cosa significano tali comandi... (impossibile capire l'AZIONE in sé)
 
 
 */
-int main(void) 
+int main(int argc, char** argv)
 {
+	// Controllo dei parametri in ingresso : 
+	// -pc per partita GIOCATORE vs COMPUTER
+	// -CC per partita COMPUTER vs COMPUTER	
+	if (argc != 2)
+	{
+		std::cerr << "Necessario un parametro: \n -pc per Partita GIOCATORE vs COMPUTER \n -cc per partita COMPUTER vs COMPUTER";
+		exit(-1);
+	}
+
+	string param = argv[1];
+	if (param != "-pc" &&
+		param != "-cc")
+	{
+		std::cerr << "Necessario un parametro: \n -pc per Partita GIOCATORE vs COMPUTER \n -cc per partita COMPUTER vs COMPUTER";
+		exit(-1);
+	}
+	
+	bool is_pc_game = (param == "-pc");
+	string target_title = is_pc_game == true ? "Player" : "Computer";
 	cout << "-------------------------------" << endl;
 	cout << "        BATTAGLIA NAVALE       " << endl;
+	cout << "       "<< target_title <<" vs Computer      " << endl;
 	cout << " Albertin, Selvestrel, Stefani " << endl;
 	cout << "-------------------------------" << endl << endl << endl;
 
-	GameManager game;
-	game.ExecCommand("XX XX", GameManager::PlayerOne);
+	//GameManager game;
+	//game.ExecCommand("XX XX", GameManager::PlayerOne);
 
 }
+
+
